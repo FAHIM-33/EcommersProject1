24018ef788821b5305582337ad6242fc4075b674
FAHIM-33 <shahriyerfahim2012@gmail.com>
2024-02-04 17:29:00 +0600

    featured added

--- src/Pages/Home/Featured/Featured.jsx
+++ src/Pages/Home/Featured/Featured.jsx
@@ -3,7 +3,16 @@ import Heading from "@/components/custom/Heading";
 import { FaChevronRight } from "react-icons/fa";
 
 
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { A11y } from 'swiper/modules';
+import { GrNext, GrPrevious } from "react-icons/gr";
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
 const Featured = () => {
+    let swiperRef;
+
     return (
         <section >
             <div className="flex justify-between bg-back my-4 py-4 px-3 shadow-md rounded-md">
@@ -11,8 +20,30 @@ const Featured = () => {
                 <button className="text-mid flex gap-2 items-center p-1">View all products <FaChevronRight className="mt-1"></FaChevronRight></button>
             </div>
 
-            <div>
-                <Card></Card>
+            <div className="relative">
+                <Swiper
+                    modules={[A11y]}
+                    spaceBetween={15}
+                    slidesPerView={5}
+                    pagination={{ clickable: true }}
+                    // onSlideChange={() => console.log('slide change')}
+                    onSwiper={(swiper) => swiperRef = swiper}
+                    className='h-full'
+                >
+                    <SwiperSlide><Card></Card></SwiperSlide>
+                    <SwiperSlide><Card></Card></SwiperSlide>
+                    <SwiperSlide><Card></Card></SwiperSlide>
+                    <SwiperSlide><Card></Card></SwiperSlide>
+                    <SwiperSlide><Card></Card></SwiperSlide>
+                    <SwiperSlide><Card></Card></SwiperSlide>
+                    <SwiperSlide><Card></Card></SwiperSlide>
+                    <SwiperSlide><Card></Card></SwiperSlide>
+
+                </Swiper>
+
+                <button className='left-0 -ml-4 h-8 w-8 top-1/2 -translate-y-1/2 z-10 absolute text-center rounded-full bg-back drop-shadow-md' onClick={() => swiperRef.slidePrev()}><GrPrevious className='mx-auto'></GrPrevious></button>
+                <button className='right-0 -mr-4 h-8 w-8 top-1/2 -translate-y-1/2 z-10 absolute text-center rounded-full bg-back drop-shadow-md' onClick={() => swiperRef.slideNext()}><GrNext className='mx-auto'></GrNext></button>
+
             </div>
         </section>
     );
--- src/components/custom/Card.jsx
+++ src/components/custom/Card.jsx
@@ -1,27 +1,34 @@
 import Rating from 'react-rating'
-import { FaStar, FaRegStar } from "react-icons/fa";
+import { FaStar, FaRegStar, FaCartPlus } from "react-icons/fa";
+
 
 const Card = () => {
     return (
-        <div className="brr p-2 w-fit bg-back rounded-md">
-            <img src="/src/assets/head2.jpg" alt="" className="w-48 mx-auto" />
+        <div className="p-3 w-full relative bg-back rounded-md shadow-md ">
+            <img src="/src/assets/head2.jpg" alt="" className="w-full h-52 object-contain mx-auto" />
             <h3 className='font-medium '>Name of Product none none none none none none</h3>
-            <div className='text-mid text-sm items-center flex gap-4'>
+            <div className='text-mid text-sm items-center flex gap-4 mt-1'>
                 <Rating
                     initialRating={3}
                     readonly
-                    fullSymbol={<FaStar></FaStar>}
+                    fullSymbol={<FaStar className='text-amber-400'></FaStar>}
                     emptySymbol={<FaRegStar></FaRegStar>}
                 />
                 <p>({20} Review)</p>
             </div>
             <span className='text-xs'>In stock</span>
-            <p className='text-xl font-semibold text-prim'>999.00$</p>
-            <div className='flex items-center justify-between'>
-                <button>Buy now</button>
-                <button>Add to cart</button>
+
+            <div className='flex justify-between'>
+                <div className='flex gap-3'>
+                    <p className='text-xl font-semibold text-prim'>999.00$</p>
+                    <del className='text-mid text-sm'>899.00</del>
+                </div>
+                <button className='border-2 p-1 border-mid text-mid rounded-md'>
+                    <FaCartPlus className='text-xl'></FaCartPlus>
+                </button>
 
             </div>
+            <p className='absolute top-3 left-3 text-[0.67rem] text-white bg-red-600 font-semibold rounded-md px-1 py-px '>{20}% discount</p>
         </div>
     );
 };
